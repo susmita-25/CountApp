b37cbd31f22c9177c3a6dfc130886ddaa810a363
susmitachavan <sushmita.c@sitsolutions.co.in>
2023-10-08 09:04:09 +0530

    task related changes

---
+++ hooks/useDarkMode.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+const useDarkMode = (initialState = false) => {
+  const [darkMode, setDarkMode] = useState(initialState);
+
+  useEffect(() => {
+    const body = document.body;
+
+    if (darkMode) {
+      body.classList.add('dark'); // Add the class for dark mode
+      body.classList.remove('light'); // Remove the class for light mode
+    } else {
+      body.classList.remove('dark'); // Remove the class for dark mode
+      body.classList.add('light'); // Add the class for light mode
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return { darkMode, toggleDarkMode };
+};
+
+export default useDarkMode;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,23 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import useCounter from '../hooks/useCounter';
-import useDarkModeToggle from '../hooks/useDarkMode';
-import { useEffect } from 'react';
+import useDarkMode from '../hooks/useDarkMode'; // Import your custom hook
 
 export default function Home() {
   const { counter, increment, decrement } = useCounter();
-  const { darkMode, setDarkMode } = useDarkModeToggle();
+  const { darkMode, toggleDarkMode } = useDarkMode();
 
-  useEffect(() => {
-    if (darkMode) {
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-    }
-  }, [darkMode]);
+  const { counter: counter1, increment: increment1, decrement: decrement1 } = useCounter();
+  const { counter: counter2, increment: increment2, decrement: decrement2 } = useCounter();
 
-  const handleToggleDarkMode = () => {
-    setDarkMode(!darkMode); // Toggle dark mode state
-  };
   return (
     <div className="container mx-auto p-4">
       <Head>
-        <title>Counter App</title>
+        <title>Counter App Task</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.16/tailwind.min.css" />
       </Head>
       <div className="mb-4">
-        <button onClick={handleToggleDarkMode} className="px-4 py-2 bg-blue-500 text-white rounded-md">
+        <button onClick={toggleDarkMode} className="button-style">
           {darkMode ? 'Disable Dark Mode' : 'Enable Dark Mode'}
         </button>
       </div>
@@ -34,14 +26,14 @@ export default function Home() {
           <h2 className="text-2xl font-semibold">Counter 1</h2>
           <div className="flex space-x-2">
             <button
-              onClick={() => decrement()}
+              onClick={() => decrement1()}
               className="px-4 py-2 bg-red-500 text-white rounded-md"
             >
               Decrement
             </button>
-            <p className="text-xl">{counter}</p>
+            <p className="text-xl">{counter1}</p>
             <button
-              onClick={() => increment()}
+              onClick={() => increment1()}
               className="px-4 py-2 bg-green-500 text-white rounded-md"
             >
               Increment
@@ -56,23 +48,23 @@ export default function Home() {
               onChange={(e) => {
                 const value = parseInt(e.target.value);
                 if (!isNaN(value)) {
-                  decrement(value);
+                  decrement2(value);
                 }
               }}
               placeholder="Decrement Value"
-              className="px-4 py-2 rounded-md"
+              className="px-4 py-2 rounded-md input"
             />
-            <p className="text-xl">{counter}</p>
+            <p className="text-xl">{counter2}</p>
             <input
               type="number"
               onChange={(e) => {
                 const value = parseInt(e.target.value);
                 if (!isNaN(value)) {
-                  increment(value);
+                  increment2(value);
                 }
               }}
               placeholder="Increment Value"
-              className="px-4 py-2 rounded-md"
+              className="px-4 py-2 rounded-md input"
             />
           </div>
         </div>
